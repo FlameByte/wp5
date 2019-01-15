ecd7b08ac3acadd4ab66099e9fd019374b55ae43
henrilindret <henri.lindret@khk.ee>
2019-01-15 11:36:56 +0200

    modified plugin `Valentine's Day Hearts` version 2.0

--- wp-content/plugins/valentines-day-floating-hearts/valentines-day.php
+++ wp-content/plugins/valentines-day-floating-hearts/valentines-day.php
@@ -1,25 +1,12 @@
 <?php
-/*
-Plugin Name: Valentine's Day Hearts
-Plugin URI: http://graphicedit.com/blog/plugin/xmas-heart/
-Description: Simple plugin that makes hearts "fall from the sky".
-Version: 2.0
-Author: Digitally Cultured
-Author URI: https://digitallycultured.com/
-
-
-	Credit to GraphicEdit (<a href="http://graphicedit.com" />GraphicEdit</a>)	
-	Copyright 2016  Digitally Cultured  (email : info@digitallycultured.com)
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-*/
+/**
+ * Plugin Name: langevad s端damed
+ * Plugin URI: http://henrilindert.ikt.khk.ee
+ * Description: This is the very first plugin i ever created.
+ * Version: 1.0
+ * Author: Henri Lindret
+ * Author URI: http://henrilindert.ikt.khk.ee
+**/
 
 define( 'DC_PLUGIN_BASENAME', plugin_basename( __FILE__ ) );
 
